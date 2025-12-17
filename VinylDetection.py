a9c4fdc1ca205460072fe64a5a26a98e5686b868
Peter Yehl <yehl.peter@gmail.com>
2025-12-17 17:25:09 -0600

    Remove Face Detection Testing

--- VinylDetection.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import torch
-import clip
-
-model, preprocess = clip.load("ViT-B/32")
-image = preprocess(Image.open("cover.jpg")).unsqueeze(0)
-
-with torch.no_grad():
-    image_features = model.encode_image(image)
